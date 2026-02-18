1f3a3f5c5d626a25ced34926e87d65c62d62c00c
Scott Sibole <scott.sibole@gmail.com>
2026-02-17 17:05:12 -0800

    suppress some type errors from h5py package

--- src/examples/view_hdf5.py
+++ src/examples/view_hdf5.py
@@ -1,8 +1,10 @@
 import h5py
 
+
 def print_datasets(name, obj):
     if isinstance(obj, h5py.Dataset):
         print(name, f"shape: {obj.shape}", f"dtype: {obj.dtype}")
+
 
 f = h5py.File("elastic_hex20.hdf5", "r")
 # view all the datasets and their shape and dtype
@@ -15,11 +17,11 @@ print(f["/states/5"].attrs["time"])
 
 # to view the data in a Dataset
 print("\n Displacement at state 5")
-print(f["/states/5/node_data/displacement/1"][:])
+print(f["/states/5/node_data/displacement/1"][:])  # type: ignore
 
 # this is simply a numpy array so we can also slice it
 print("\n Displacement at state 5 of first 2 nodes")
-print(f["/states/5/node_data/displacement/1"][0:2,:])
+print(f["/states/5/node_data/displacement/1"][0:2, :])  # type: ignore
 # the nice thing about this though is HDF5 uses lazy loading
 # which means that the data is not loaded into memory until it is accessed
 # this is useful for large datasets that do not fit into memory
