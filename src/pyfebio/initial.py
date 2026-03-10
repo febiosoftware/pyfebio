086c376ba889cb834c050ad549f7a101492fdff5
Scott Sibole <scott.sibole@gmail.com>
2026-03-10 11:41:19 -0700

    initial solute concentration and fluid pressure conditions added

--- src/pyfebio/initial.py
+++ src/pyfebio/initial.py
@@ -20,11 +20,25 @@ class InitialPrestrain(BaseXmlModel, validate_assignment=True):
     reset: Literal[0, 1] = element(default=1)
 
 
-class Initial(BaseXmlModel, validate_assignment=True):
-    all_initial_conditions: list[InitialVelocity | InitialPrestrain] = element(default=[], tag="ic")
+class InitialFluidPressure(BaseXmlModel, tag="ic", validate_assignment=True):
+    type: Literal["initial fluid pressure"] = attr(default="initial fluid pressure", frozen=True)
+    node_set: str = attr()
+    value: float = element()
 
-    def add_initial_condition(self, new_initial_condition: InitialVelocity | InitialPrestrain):
-        assert isinstance(new_initial_condition, (InitialVelocity, InitialPrestrain)), (
-            "new_initial_condition must be an InitialVelocity or InitialPrestrain"
-        )
+
+class InitialConcentration(BaseXmlModel, tag="ic", validate_assignment=True):
+    type: Literal["initial concentration"] = attr(default="initial concentration", frozen=True)
+    node_set: str = attr()
+    value: float = element()
+    dof: str = element()
+
+
+InitialConditionType = InitialVelocity | InitialPrestrain | InitialConcentration | InitialFluidPressure
+
+
+class Initial(BaseXmlModel, validate_assignment=True):
+    all_initial_conditions: list[InitialConditionType] = element(default=[], tag="ic")
+
+    def add_initial_condition(self, new_initial_condition: InitialConditionType):
+        assert isinstance(new_initial_condition, InitialConditionType), "new_initial_condition must be an InitialConditionType"
         self.all_initial_conditions.append(new_initial_condition)
